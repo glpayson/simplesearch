0360bd0a5badae842152b913ba0685e163cfb6b3
Lamar Payson <glpayson@gmail.com>
2019-06-25 16:56:53 -0400

    ..

--- src/main/java/com/exercise/search/Main.java
+++ src/main/java/com/exercise/search/Main.java
@@ -1,19 +1,17 @@
 package com.exercise.search;
 
-import com.exercise.search.corpus.CorpusParser;
-import com.exercise.search.index.Index;
-import com.exercise.search.index.IndexFactory;
-import com.exercise.search.index.TFIDFCalculator;
 import com.exercise.search.io.DirectoryReader;
 
 public class Main {
 	public static void main(String[] args) {
+		String testDir = "src/test/resources/documents";
 		DirectoryReader dirReader = new DirectoryReader();
-		CorpusParser corpusParser = new CorpusParser();
-		TFIDFCalculator tfidfCalculator = new TFIDFCalculator();
 
-		Index index = new IndexFactory(dirReader, corpusParser, tfidfCalculator)
-				.createIndex("src/main/resources/documents");
-		System.out.println(index.documents().size());
+		// Expected TF-IDF scores for src/test/resources/documents/a.txt
+		//	the, 0.0
+		//	mouse, 0.183102
+		//	with, 0.183102
+		//	cat, 0.183102
+		//	played, 0.183102
 	}
 }
