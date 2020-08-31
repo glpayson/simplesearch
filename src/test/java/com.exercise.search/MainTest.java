6bc493f749e19ec6b6efbe6d319b9104f26878c2
glpayson <lamar.payson@careerbuilder.com>
2020-08-31 11:34:34 -0400

    Add test.

---
+++ src/test/java/com.exercise.search/MainTest.java
@@ -0,0 +1,28 @@
+package com.exercise.search;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
+
+import com.exercise.search.io.DirectoryReader;
+import org.junit.jupiter.api.Test;
+
+import com.exercise.search.*;
+
+class MainTest {
+	@Test
+	void tfIdf() {
+		// Read files from disk
+		String testDir = "src/test/resources/documents";
+		DirectoryReader dirReader = new DirectoryReader();
+		Map<String, String> fileNameToText = dirReader.read(testDir);
+
+		// Calculate tf-idf for "mouse" in document "a.txt"
+		Index myIndex = new Index(fileNameToText);
+		double actual = myIndex.tfidfScore("mouse", "a.txt");
+		double expected = 0.183102;
+		
+		assertEquals(expected, actual, 0.00001);
+	}
+}
